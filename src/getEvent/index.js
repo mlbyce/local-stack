a825196df52c16d42bfaabadac9bddd09ffab2a8
Mike Byce <mike@metageek.net>
2020-01-10 22:15:31 -0700

    debugging

--- src/getEvent/index.js
+++ src/getEvent/index.js
@@ -1,6 +1,13 @@
 exports.handler = async (event, context) => {
   // Log the event argument for debugging and for use in local development.
-  console.log(JSON.stringify(event, undefined, 2));
+  //console.log(JSON.stringify(event, undefined, 2));
+  console.log('HEY: I Got This');
 
-  return event;
+  //return event;
+
+  return {
+        statusCode: 200,
+        headers: { 'Content-Type': 'application/json' },
+        body: 'At Least This Works'
+    };
 };
